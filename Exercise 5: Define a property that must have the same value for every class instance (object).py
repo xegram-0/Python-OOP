331b1ab0f96d066be24b04211f7f15f9d703a66b
Hong Nhan <nguyenhongnhan9a5@gmail.com>
2024-05-24 17:25:27 +0700

    Solved 5

--- Exercise 5: Define a property that must have the same value for every class instance (object).py
+++ Exercise 5: Define a property that must have the same value for every class instance (object).py
@@ -1,1 +1,43 @@
 # Exercise 5: Define a property that must have the same value for every class instance (object)
+# Define a class attribute”color” with a default value white. I.e., Every Vehicle should be white.
+
+# Use the following code for this exercise.
+
+# class Vehicle:
+
+#     def __init__(self, name, max_speed, mileage):
+#         self.name = name
+#         self.max_speed = max_speed
+#         self.mileage = mileage
+
+# class Bus(Vehicle):
+#     pass
+
+# class Car(Vehicle):
+#     pass
+
+# Expected Output:
+
+# Color: White, Vehicle name: School Volvo, Speed: 180, Mileage: 12
+# Color: White, Vehicle name: Audi Q5, Speed: 240, Mileage: 18
+
+
+class Vehicle:
+    color = "White"
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+class Bus(Vehicle):
+    pass
+
+class Car(Vehicle):
+    pass
+
+schoolBus = Bus("School volvo", 180, 12)
+audi = Car("Audi Q5", 240, 18)
+
+
+print(f"{schoolBus.color}, {schoolBus.name}, {schoolBus.max_speed}, {schoolBus.mileage}")
+print(f"{audi.color}, {audi.name}, {audi.max_speed}, {audi.mileage}")
