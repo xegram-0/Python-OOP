2f80ebdaae56f367db573ddb086138aca2a9b792
Hong Nhan <nguyenhongnhan9a5@gmail.com>
2024-05-25 16:17:37 +0700

    Solved 8

--- Exercise 8: Determine if School_bus is also an instance of the Vehicle class.py
+++ Exercise 8: Determine if School_bus is also an instance of the Vehicle class.py
@@ -1,1 +1,29 @@
 # Exercise 8: Determine if School_bus is also an instance of the Vehicle class
+# Given:
+
+# class Vehicle:
+#     def __init__(self, name, mileage, capacity):
+#         self.name = name
+#         self.mileage = mileage
+#         self.capacity = capacity
+
+# class Bus(Vehicle):
+#     pass
+
+# School_bus = Bus("School Volvo", 12, 50)
+
+class Vehicle:
+    def __init__(self, name, mileage, capacity):
+        self.name = name
+        self.mileage = mileage
+        self.capacity = capacity
+
+class Bus(Vehicle):
+    pass
+
+School_bus = Bus("School Volvo", 12, 50)
+
+if isinstance(School_bus,Vehicle):
+    print("School_bus is an instance of Vehicile.")
+else:
+    print("School_bus is not an instance of Vehicle.")
