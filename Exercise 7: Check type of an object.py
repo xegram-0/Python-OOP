03c6fbe36c2a28e2f360a4762c189ed6919d7c86
Hong Nhan <nguyenhongnhan9a5@gmail.com>
2024-05-25 16:12:36 +0700

    Solved 7

--- Exercise 7: Check type of an object.py
+++ Exercise 7: Check type of an object.py
@@ -1,1 +1,27 @@
 # Exercise 7: Check type of an object
+# Write a program to determine which class a given Bus object belongs to.
+
+# Given:
+
+# class Vehicle:
+#     def __init__(self, name, mileage, capacity):
+#         self.name = name
+#         self.mileage = mileage
+#         self.capacity = capacity
+
+# class Bus(Vehicle):
+#     pass
+
+# School_bus = Bus("School Volvo", 12, 50)
+
+class Vehicle:
+    def __init__(self, name, mileage, capacity):
+        self.name = name
+        self.mileage = mileage
+        self.capacity = capacity
+
+class Bus(Vehicle):
+    pass
+
+School_bus = Bus("School Volvo", 12, 50)
+print(type(School_bus))
